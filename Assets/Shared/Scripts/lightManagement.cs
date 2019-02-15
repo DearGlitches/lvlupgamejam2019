a08efd647a2828ae47fb13921641a13300ca9f92
Adi <adrian.enderlin@bluewin.ch>
2019-02-15 21:28:42 +0100

    prefab claudine + sound management

--- Assets/Shared/Scripts/lightManagement.cs
+++ Assets/Shared/Scripts/lightManagement.cs
@@ -22,6 +22,22 @@ public class lightManagement : MonoBehaviour
     void Start()
     {
         endTargetStartPosY = endTarget.transform.position.y;
+
+        if (underwater1)
+        {
+            underwater1.volume = 1;
+            underwater1.Play();
+        }
+        if (underwater2)
+        {
+            underwater2.volume = 0;
+            underwater2.Play();
+        }
+        if (underwater3)
+        {
+            underwater3.volume = 0;
+            underwater3.Play();
+        }
     }
 
     // Update is called once per frame
@@ -29,7 +45,45 @@ public class lightManagement : MonoBehaviour
     {
         if (player.transform.position.y != 0) {
             float delta = (endTarget.transform.position.y - player.transform.position.y) / endTargetStartPosY * multiplier / 10;
+            float deltasound1 = (endTarget.transform.position.y - player.transform.position.y) / endTargetStartPosY * multiplier / 10;
             lightTarget.intensity = delta*10;
+
+            if (delta > .80f)
+            {
+                underwater1.volume = 1;
+            }
+            else if (delta > .7f)
+            {
+                underwater1.volume = 0.6f;
+                underwater2.volume = 0.3f;
+            }
+            else if (delta > .6f)
+            {
+                underwater1.volume = 0.3f;
+                underwater2.volume = 0.6f;
+            }
+            else if (delta > .5f)
+            {
+                underwater1.volume = 0.0f;
+                underwater2.volume = 1;
+            }
+            else if (delta > .3f)
+            {
+                underwater2.volume = 0.6f;
+                underwater3.volume = 0.3f;
+            }
+            else if (delta > .2f)
+            {
+                underwater2.volume = 0.3f;
+                underwater3.volume = 0.6f;
+            }
+            else if (delta > .1f)
+            {
+                underwater2.volume = 0.0f;
+                underwater3.volume = 1f;
+            }
+          
+
             camera.backgroundColor = Color.Lerp(color1, color2, delta);
         }
     }
