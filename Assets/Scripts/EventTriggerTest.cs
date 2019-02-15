2ba1f67b0761197880f80492e81c6b398a7cfb60
wengerol <olivier.wenger@hotmail.fr>
2019-02-15 14:06:30 +0100

    base lvl1

---
+++ Assets/Scripts/EventTriggerTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTriggerTest : MonoBehaviour {
+
+
+    void Update () {
+        if (Input.GetKeyDown ("q"))
+        {
+            EventManager.TriggerEvent ("test", 1);
+        }
+
+        if (Input.GetKeyDown ("o"))
+        {
+            EventManager.TriggerEvent ("Spawn", 2);
+        }
+
+        if (Input.GetKeyDown ("p"))
+        {
+            EventManager.TriggerEvent ("Destroy", 3);
+        }
+
+        if (Input.GetKeyDown ("x"))
+        {
+            EventManager.TriggerEvent ("Junk", 4);
+        }
+    }
+}
